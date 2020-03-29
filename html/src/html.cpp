89a8f31da1d95f07d1f230cb88f41c8421d8d994
Ray Redondo <rdrpenguin04@gmail.com>
2020-03-28 21:43:41 -0500

    Create LICENSE and change placeholder file for `file`

---
+++ html/src/html.cpp
@@ -0,0 +1,7 @@
+// std-browser: The standardized browser implementation
+// From the YouTube series (link coming soon!)
+// Copyright (C) 2020 Lightning Creations <lightningcreationshw@gmail.com>
+//
+// This file is part of std-browser and is licensed under the GNU General Public
+// License 3.0, or later, with the additional special exception to link portions
+// of this program with the OpenSSL library. See LICENSE for more details.
